d5c45074391b98b04977033a0acf9fa038a3cb54
Julien Pillaud <julien.pillaud@autajon.com>
2024-08-02 15:40:02 +0200

    update

--- app/repository/interface.py
+++ app/repository/interface.py
@@ -1,6 +1,9 @@
 import uuid
 from typing import Any, Protocol
 
+from sqlalchemy import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
+
 from app.roles.schemas import RoleDetail
 from app.users.schemas import UserDetail
 
@@ -17,3 +20,18 @@ class AbstractRoleRepository(Protocol):
     def get_all(self) -> list[RoleDetail]: ...
 
     def get_by_code(self, code: str) -> RoleDetail | None: ...
+
+
+class Base(DeclarativeBase):
+    """Custom declarative base for SQLAlchemy
+
+    # https://docs.sqlalchemy.org/en/20/orm/declarative_mixins.html#augmenting-the-base
+    """
+
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )

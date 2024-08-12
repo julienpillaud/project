1ac8fc07fc77935909220aa1312cf878b51334c6
Julien Pillaud <julien.pillaud@gmail.com>
2024-08-12 09:55:08 +0200

    update

--- app/users/routes.py
+++ app/users/routes.py
@@ -27,9 +27,9 @@ def get_users(service: ServiceDependency) -> Any:
 
 @router.get("/{user_id}", response_model=UserDetail)
 def get_user(service: ServiceDependency, user_id: uuid.UUID) -> Any:
-    user = service.get_user(user_id=user_id)
-    if not user:
+    if user := service.get_user(user_id=user_id):
+        return user
+    else:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
         )
-    return user
